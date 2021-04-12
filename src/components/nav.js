6138c6845ab634473e8f66e59978865107415bb9
Desi <desi.devaul@gmail.com>
2021-04-12 16:46:24 -0400

    changed routes

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,8 +6,8 @@ export default class Nav extends React.Component {
   render() {
     return (
       <div>
-        <a href="/">home</a>
-        <a href="/about">about</a>
+        <a href="/music-blog-frontend">home</a>
+        <a href="/music-blog-frontend/about">about</a>
       </div>
       
     )
